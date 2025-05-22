d34359b81a4a4336a19cb001dfe6c0a0d6b9068e
prakashm2006 <prakashmeenashisundaram23sit@vetias.ac.in>
2025-05-22 15:57:07 +0530

    Add files via upload

---
+++ Day 10/reverse-array-using-pointer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void reverseArray(int *arr, int size) 
+{
+    int *start = arr;
+    int *end = arr + size - 1;
+    int temp;
+    while (start < end) 
+    {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) 
+    {
+        printf("%d ", arr[i]); // 1 2 3 4 5
+    }
+
+    reverseArray(arr, size);
+    printf("\nReversed array: ");
+    for (int i = 0; i < size; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
